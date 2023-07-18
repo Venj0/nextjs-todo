1ef6422c12b4aac7a31b69e958cc87a4af0fc31d
venj0s <vens.projects@gmail.com>
2023-07-18 13:31:08 +0200

    remove unused files

--- services/getAllTodos.ts
+++
@@ -1,8 +0,0 @@
-import { Urls } from '@/services/urls'
-import { Todo } from '@/types/todo'
-import axios from 'axios'
-
-export const getAllTodos = async (): Promise<Todo[]> => {
-	const res = await axios.get<Todo[]>(`${Urls.getAllTodo}/?q="sdgasdg"`)
-	return res.data
-}
